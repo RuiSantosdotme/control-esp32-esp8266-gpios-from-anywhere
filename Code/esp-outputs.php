b169857df059e5121359d97b0a88d88618edcb8b
Rui Santos <randomnerdtutorials@gmail.com>
2020-01-23 10:55:14 +0000

    Update esp-outputs.php

--- Code/esp-outputs.php
+++ Code/esp-outputs.php
@@ -8,7 +8,6 @@
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 -->
-
 <?php
     include_once('esp-database.php');
 

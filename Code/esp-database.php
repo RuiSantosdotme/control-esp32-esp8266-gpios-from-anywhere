40b38bbd493d734b150192f2c8e6ea2518d3fe06
Rui Santos <randomnerdtutorials@gmail.com>
2020-01-23 10:55:04 +0000

    Update esp-database.php

--- Code/esp-database.php
+++ Code/esp-database.php
@@ -8,7 +8,6 @@
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 -->
-
 <?php
     $servername = "localhost";
     // Your Database name

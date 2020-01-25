467fbab41e38dbea7dbd2f5d7b86cd40e59e9bf5
Rui Santos <randomnerdtutorials@gmail.com>
2020-01-25 15:35:43 +0000

    Update esp-database.php

--- Code/esp-database.php
+++ Code/esp-database.php
@@ -1,13 +1,3 @@
-<!--
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/control-esp32-esp8266-gpios-from-anywhere/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
--->
 <?php
     $servername = "localhost";
     // Your Database name
